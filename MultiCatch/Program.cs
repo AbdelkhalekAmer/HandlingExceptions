07c014509b09c335eecd8986441728302cca34d1
Abdelkhalek Amer <Abdelkhalek.Amer@integrant.com>
2020-01-12 17:11:32 +0200

    .

---
+++ MultiCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MultiCatch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				throw new NotImplementedException();
+			}
+			catch (DivideByZeroException ex)
+			{
+				Console.WriteLine($"DivideByZeroException: {ex.Message}");
+			}
+			catch (NotImplementedException ex)
+			{
+				Console.WriteLine($"NotImplementedException: {ex.Message}");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Exception: {ex.Message}");
+			}
+		}
+
+	}
+}
